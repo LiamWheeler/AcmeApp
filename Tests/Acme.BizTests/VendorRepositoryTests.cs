55b4fe49eff07b5da74ef9d0b17787e9842e6fcb
liam wheeler <liam_wheeler@hotmail.com>
2019-05-31 10:07:30 +0100

    Generic retrieveValue and test

---
+++ Tests/Acme.BizTests/VendorRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class VendorRepositoryTests
+    {
+        [TestMethod()]
+        public void RetrieveValueTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = 42;
+
+            //Act
+            var actual = repository.RetrieveValue<int>("Select ...", 42);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
